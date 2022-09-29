b39e774889103bf9a56dada3217797e0fd030926
Efrain May <efrain_may_75@hotmail.com>
2022-09-29 00:15:50 -0500

    initial commit

---
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -0,0 +1,65 @@
+<template>
+    <h1>About Page</h1>
+    <hr>
+
+    <h2>{{name}}</h2>
+    
+    <button @click="onChangeName">
+        Cambiar nombre
+    </button>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            name: 'Efrain'
+        }
+    },
+    methods: {
+        onChangeName(){
+            this.name = 'Eduardo'
+        }
+    },
+    beforeCreate(){
+        console.log('beforeCreate')
+    },
+    created(){
+        this.name = 'Juan carlos'
+        console.log('created')
+        //re recomiendas hacer peticiones HTTP
+    },
+    beforeMount(){
+        console.log('beforeMount')
+    },
+    mounted(){
+        console.log('mounted')
+    },
+    beforeUpdate(){
+        console.log('beforeUpdate')
+    },
+    updated(){
+        console.log('updated')
+    },
+    activated(){
+        console.log('activated')
+    },
+    deactivated(){
+        console.log('deactivated')
+    },
+    beforeUnmount(){
+        console.log('beforeUnmount')
+    },
+    unmounted(){
+        console.log('unmounted')
+    },
+    errorCaptured(){
+        console.log('errorCaptured')
+    },
+    renderTracked(){
+        console.log('renderTracked')
+    },
+    renderTriggered(){
+        console.log('renderTriggered')
+    },
+}
+</script>
---
+++ src/router/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+    { 
+        path: '/', 
+        component: () => import(/* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage') 
+    },
+    { 
+        path: '/about', 
+        component: () => import(/* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage') 
+    },
+    { 
+        path: '/id', 
+        component: () => import(/* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage') 
+    },
+    { 
+        path: '/:pathMatch(.*)*', 
+        component: () => import(/* webpackChunkName: "NoPageFound" */ '@/modules/shared/pages/NoPageFound') 
+    },
+]
+
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
