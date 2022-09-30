5ccb8ce4c9707fe37e6f7b38ae9ded6fd4d42830
Efrain May <efrain_may_75@hotmail.com>
2022-09-29 23:27:04 -0500

    guard asincrono

--- src/router/router.js
+++ src/router/router.js
@@ -71,7 +71,7 @@ const router = createRouter({
 })
 
 //guard global -sincrono
-router.beforeEach((to, from, next) => {
+/* router.beforeEach((to, from, next) => {
     console.log({to, from, next})
 
     const random = Math.random() * 100
@@ -82,6 +82,26 @@ router.beforeEach((to, from, next) => {
         console.log(random, 'bloqueado por el beforeEachGuard')
         next({name: 'pokemon-home'})
     }
+}) */
+
+const canAccess = () => {
+    return new Promise((resolve) => {
+        const random = Math.random() * 100
+        if(random > 50){
+            console.log('Autenticado - canAccess')
+            resolve(true)
+        }else{
+            console.log(random, 'bloqueado por el beforeEachGuard-canAccess')
+            resolve(false)
+        }
+    })
+}
+
+router.beforeEach(async(to, from, next) => {
+    const authorized = await canAccess()
+    authorized
+        ? next()
+        : next({name: 'pokemon-home'})
 })
 
 export default router
